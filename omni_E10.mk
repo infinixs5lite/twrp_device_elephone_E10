6c32dbc4354e5bf6f29780070dd98cb3c63da179
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-12-21 20:38:33 +0100

    E10: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: elephone Model: E10 Script version: 3.0.0

---
+++ omni_E10.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from E10 device
+$(call inherit-product, device/elephone/E10/device.mk)
+
+PRODUCT_DEVICE := E10
+PRODUCT_NAME := omni_E10
+PRODUCT_BRAND := Elephone
+PRODUCT_MODEL := E10
+PRODUCT_MANUFACTURER := elephone
+
+PRODUCT_GMS_CLIENTID_BASE := android-elephone
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_k62v1_64_bsp-user 10 QP1A.190711.020 mp1V948 release-keys"
+
+BUILD_FINGERPRINT := Elephone/E10_EEA/E10:10/QP1A.190711.020/mp1V948:user/release-keys
