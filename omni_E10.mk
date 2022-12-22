4606f0ef77ed68bf6e6c1f4d29e6f28075c98443
infinixs5lite <kelvinchinedu97@gmail.com>
2022-12-22 21:21:55 +0100

    Update omni_E10.mk

--- omni_E10.mk
+++ omni_E10.mk
@@ -1,8 +1,13 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from E10 device
 $(call inherit-product, device/elephone/E10/device.mk)
