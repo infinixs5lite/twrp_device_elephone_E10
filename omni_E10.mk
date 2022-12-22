4b6255b3ab95733de20d333829d8a74a2e0e5a05
infinixs5lite <kelvinchinedu97@gmail.com>
2022-12-22 19:29:07 +0100

    Update omni_E10.mk

--- omni_E10.mk
+++ omni_E10.mk
@@ -1,21 +1,7 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Inherit from E10 device
-$(call inherit-product, device/elophone/E10/device.mk)
-
-# Inherit some common omni stuff.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_DEVICE := E10
