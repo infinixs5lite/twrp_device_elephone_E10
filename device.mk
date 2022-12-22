a82298faf3933c0b638414a88ecfacd58445b413
infinixs5lite <kelvinchinedu97@gmail.com>
2022-12-22 21:00:55 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -9,16 +9,3 @@ LOCAL_PATH := device/elephone/E10
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-PRODUCT_PACKAGES += \
-    bootctrl.mt6765
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
